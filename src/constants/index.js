57d8b38a9dda453886b8be2f2f377cb2afa4d5ef
assem <gddfggggdadg12@gmail.com>
2024-06-28 14:59:08 +0300

    first commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -112,7 +112,7 @@ export const footerLinks = [
         title: "Get in touch",
         links: [
             { name: "customer@nike.com", link: "mailto:customer@nike.com" },
-            { name: "+92554862354", link: "tel:+92554862354" },
+            { name: "+20 102 984 5275", link: "tel:+20 102 984 5275" },
         ],
     },
 ];
